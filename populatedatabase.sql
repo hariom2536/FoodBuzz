dbc8ef1d67344e7b8e3ad64f7de8f462ed724e0b
Michael <mgardner94@live.com>
2015-10-14 20:12:28 -0400

    Database population v1.

---
+++ populatedatabase.sql
@@ -0,0 +1,16 @@
+INSERT INTO Registered_Users(
+    VALUES ('mikeygmikeyg','lebron23','mcg5215@psu.edu','Michael C. Gardner',02/24/1994,10000000,'M','12345','400012345654'),
+    VALUES ('zhucity','password2','miz5000@psu.edu','Matthew Zhu',08/25/1994,100,'M','123455','400012345634'),
+    VALUES ('gamergirl68','password3','gamezzz@gmail.com','Lanisha H. Jefferson',03/25/1990,0,'M','123455','4000123456254'),
+    VALUES ('LeBronJames23','lebrongoat','lebron@cavs.com','Lebron James',12/30/1984,24000000,'M','1234556','4000123456223'),
+    VALUES ('jeez24','geeze12','enis@gmail.com','Enis H. Gorder','Enis',12/12/1995,0,'M','121324','3243532455'),
+    VALUES ('derrickrose','injury23','maybenextyear@bulls.com','Derrick Rose',08/04/1988,12000000,'M','232421','40002234562'),
+    VALUES ('kateupton1000','password33','kateupton@yahoo.com','Katelyn Upton',06/10/1992,200000,'F','12233455','40121234562'),
+    VALUES ('barackobama12','president12','bo@whitehouse.gov','Barrack H. Obama',08/04/1961,4000000,'M','122133455','40121232562'),
+    VALUES ('kyrieirving8','cavs2','kyrie@cavs.com','Kyrie Iriving',03/23/1992,12000000,'M','1221233455','401212432562'),
+    VALUES ('serenawilliams','tennis13','serena@gmail.com','Serena Willaims',09/26/1981,5000000,'M','12213323455','4012124332562'),
+    VALUES ('iggyazelea','president12','bo@whitehouse.gov','Barrack H. Obama',08/04/1961,4000000,'M','122133455','40121232562'),
+    
+    
+    );
+    
