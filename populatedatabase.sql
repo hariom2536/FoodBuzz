39ccc2d297b7b125ad606eed09bedfb15f83857e
Michael <mgardner94@live.com>
2015-10-14 21:22:27 -0400

    v4

--- populatedatabase.sql
+++ populatedatabase.sql
@@ -1,4 +1,4 @@
-INSERT INTO Registered_Users(
+INSERT INTO Registered_User(
     VALUES ('mikeygmikeyg','lebron23','mcg5215@psu.edu','Michael C. Gardner',02/24/1994,10000000,'M','12345','400012345654'),
     VALUES ('zhucity','password2','miz5000@psu.edu','Matthew Zhu',08/25/1994,100,'M','123455','400012345634'),
     VALUES ('gamergirl68','password3','gamezzz@gmail.com','Lanisha H. Jefferson',03/25/1990,0,'M','123455','4000123456254'),
@@ -15,7 +15,60 @@ INSERT INTO Registered_Users(
     VALUES ('orderback','noun','indvidual@faciliate.com','Ordering O. Food',01/01/1991,3000000,'M','1221331455','401212322562'),
     VALUES ('Karl22','red12','karlmarx@marx.com','Karl Marx',05/05/1818,2000,'M','1221331455','401212322562'),
 
+    );
+    
+    
+    INSERT INTO Sale_Item(
+    VALUES ('100001','Kitkat','Chocolate')
+    VALUES ('100002','Marshmallow','Candy')
+    VALUES ('100003','Lollipop','Candy')
+    VALUES ('100004','Hershey','Chocolate')
+    VALUES ('100005','Bacon','Meat')
+    VALUES ('100006','Ribs','Meat')
+    VALUES ('100007','Chicken','Meat')
+    VALUES ('100008','Sausage','Meat')
+    VALUES ('100009','Egg','Meat')
+    VALUES ('100010','Cheese','Milk')
+    VALUES ('100011','Snapple','Juice')
+    VALUES ('100012','Cola','Soda')
+    VALUES ('100013','Cookie','Home-made')
+    VALUES ('100014','Pastas','Home-made')
+    VALUES ('100015','Boneless-chicken','Home-made')
+    VALUES ('100016','M&M','Candy')
+    VALUES ('100017','Lays','Chip')
+    VALUES ('100018','Mocha','Coffee')
+    VALUES ('100019','Oreo','chocolate cookie')
+    VALUES ('100020','Spinach Pizza','spinach and fita')
+    VALUES ('100021','Honolulu Pizza','pineapple and meat')
+    VALUES ('100022','Philly Pizza','cheese steak')
+    VALUES ('100023','Hawaiian Pizza','jalapeno and ham')
+    VALUES ('100024','Pasta Primavera','spinach and tomatoes')
+    VALUES ('100025','Italian Pasta','italian sausage')
+    VALUES ('100026','Alfredo Pasta','chicken')
+    VALUES ('100027','Wings','Marinated over-baked')
+    VALUES ('100028','Cheesy Bread','oven-baked')
+    VALUES ('100029','Candy Apples','apples with coating')
+    VALUES ('100030','Champagne Cake','lovely white cake') 
+    VALUES ('100031','Mummy Dogs','hotdogs')
+    VALUES ('100032','Pumpkin Smoothies','pumpkin')
+    VALUES ('100033','Chocolate Spiders','chocolate')
+    VALUES ('100034','Pumpkin Cookies','Cookies')
+    VALUES ('100035','Chip Bread Pudding','chocolaty-banana')
+    VALUES ('100036','Pumpkin Seeds','sweet')
+    VALUES ('100037','Peanut Butter','chocolate')
+    VALUES ('100038','Lemon Cookies','taste like lemon')
+    VALUES ('100039','Shortbread Cookie','chocolate-dipped')
+    VALUES ('100040','Fish Dry Pot','fish and vegetables')
+    VALUES ('100041','Sliced Flounder','chili sauce')
+    VALUES ('100042','French Fries','new baked')
+    VALUES ('100043','Cooked Scallop','dry cooked')
+    VALUES ('100044','Fries Chicken Wrap','with tomatoes')
+    VALUES ('100045','Brisket Sandwich','BBQ beef')
+    VALUES ('100046','Curry Chicken','red curry')
+    VALUES ('100047','Curry Beef','green curry')
+    VALUES ('100048','Salad','tomato and cabbage')
+    VALUES ('100049','red bean ice cream','red bean')
+    VALUES ('100050','fried banana','with ice cream')
     
     
     );
-    
