47aca0412b043b0924092156d9023c8382b19d8e
Michael <mgardner94@live.com>
2015-10-28 18:30:44 -0400

    fixed syntax

--- populatedatabase.sql
+++ populatedatabase.sql
@@ -1,74 +1,76 @@
 INSERT INTO Registered_User(
-    VALUES ('mikeygmikeyg','lebron23','mcg5215@psu.edu','Michael C. Gardner',02/24/1994,10000000,'M','12345','400012345654'),
-    VALUES ('zhucity','password2','miz5000@psu.edu','Matthew Zhu',08/25/1994,100,'M','123455','400012345634'),
-    VALUES ('gamergirl68','password3','gamezzz@gmail.com','Lanisha H. Jefferson',03/25/1990,0,'M','123455','4000123456254'),
-    VALUES ('LeBronJames23','lebrongoat','lebron@cavs.com','Lebron James',12/30/1984,24000000,'M','1234556','4000123456223'),
-    VALUES ('jeez24','geeze12','enis@gmail.com','Enis H. Gorder','Enis',12/12/1995,0,'M','121324','3243532455'),
-    VALUES ('derrickrose','injury23','maybenextyear@bulls.com','Derrick Rose',08/04/1988,12000000,'M','232421','40002234562'),
-    VALUES ('kateupton1000','password33','kateupton@yahoo.com','Katelyn Upton',06/10/1992,200000,'F','12233455','40121234562'),
-    VALUES ('barackobama12','president12','bo@whitehouse.gov','Barrack H. Obama',08/04/1961,4000000,'M','122133455','40121232562'),
-    VALUES ('kyrieirving8','cavs2','kyrie@cavs.com','Kyrie Iriving',03/23/1992,12000000,'M','1221233455','401212432562'),
-    VALUES ('serenawilliams','tennis13','serena@gmail.com','Serena Willaims',09/26/1981,5000000,'F','12213323455','4012124332562'),
-    VALUES ('iggyazalea','music70','iggy@bing.com','Iggy Azalea',06/07/1990,15000000,'F','122133455','40121232562'),
-    VALUES ('fettywap1738','ayyyyyy','fetty@gmail.com','Fetty H. Wap',06/07/1991,3000000,'M','1221331455','401212322562'),
-    VALUES ('rubenslikk','fishmix','astroslikk@metrozu.com','Ruben',06/07/1989,3300000,'M','1210331455','401242322562'),
-    VALUES ('orderback','noun','indvidual@faciliate.com','Ordering O. Food',01/01/1991,3000000,'M','1221331455','401212322562'),
-    VALUES ('Karl22','red12','karlmarx@marx.com','Karl Marx',05/05/1818,2000,'M','1221331455','401212322562'),
+    VALUES
+     ('mikeygmikeyg','lebron23','mcg5215@psu.edu','Michael C. Gardner',02/24/1994,10000000,'M','12345','400012345654'),
+     ('zhucity','password2','miz5000@psu.edu','Matthew Zhu',08/25/1994,100,'M','123455','400012345634'),
+     ('gamergirl68','password3','gamezzz@gmail.com','Lanisha H. Jefferson',03/25/1990,0,'M','123455','4000123456254'),
+     ('LeBronJames23','lebrongoat','lebron@cavs.com','Lebron James',12/30/1984,24000000,'M','1234556','4000123456223'),
+     ('jeez24','geeze12','enis@gmail.com','Enis H. Gorder','Enis',12/12/1995,0,'M','121324','3243532455'),
+     ('derrickrose','injury23','maybenextyear@bulls.com','Derrick Rose',08/04/1988,12000000,'M','232421','40002234562'),
+     ('kateupton1000','password33','kateupton@yahoo.com','Katelyn Upton',06/10/1992,200000,'F','12233455','40121234562'),
+     ('barackobama12','president12','bo@whitehouse.gov','Barrack H. Obama',08/04/1961,4000000,'M','122133455','40121232562'),
+     ('kyrieirving8','cavs2','kyrie@cavs.com','Kyrie Iriving',03/23/1992,12000000,'M','1221233455','401212432562'),
+     ('serenawilliams','tennis13','serena@gmail.com','Serena Willaims',09/26/1981,5000000,'F','12213323455','4012124332562'),
+     ('iggyazalea','music70','iggy@bing.com','Iggy Azalea',06/07/1990,15000000,'F','122133455','40121232562'),
+     ('fettywap1738','ayyyyyy','fetty@gmail.com','Fetty H. Wap',06/07/1991,3000000,'M','1221331455','401212322562'),
+     ('rubenslikk','fishmix','astroslikk@metrozu.com','Ruben',06/07/1989,3300000,'M','1210331455','401242322562'),
+     ('orderback','noun','indvidual@faciliate.com','Ordering O. Food',01/01/1991,3000000,'M','1221331455','401212322562'),
+     ('Karl22','red12','karlmarx@marx.com','Karl Marx',05/05/1818,2000,'M','1221331455','401212322562'),
 
     );
     
     
     INSERT INTO Sale_Item(
-    VALUES ('100001','Kitkat','Chocolate')
-    VALUES ('100002','Marshmallow','Candy')
-    VALUES ('100003','Lollipop','Candy')
-    VALUES ('100004','Hershey','Chocolate')
-    VALUES ('100005','Bacon','Meat')
-    VALUES ('100006','Ribs','Meat')
-    VALUES ('100007','Chicken','Meat')
-    VALUES ('100008','Sausage','Meat')
-    VALUES ('100009','Egg','Meat')
-    VALUES ('100010','Cheese','Milk')
-    VALUES ('100011','Snapple','Juice')
-    VALUES ('100012','Cola','Soda')
-    VALUES ('100013','Cookie','Home-made')
-    VALUES ('100014','Pastas','Home-made')
-    VALUES ('100015','Boneless-chicken','Home-made')
-    VALUES ('100016','M&M','Candy')
-    VALUES ('100017','Lays','Chip')
-    VALUES ('100018','Mocha','Coffee')
-    VALUES ('100019','Oreo','chocolate cookie')
-    VALUES ('100020','Spinach Pizza','spinach and fita')
-    VALUES ('100021','Honolulu Pizza','pineapple and meat')
-    VALUES ('100022','Philly Pizza','cheese steak')
-    VALUES ('100023','Hawaiian Pizza','jalapeno and ham')
-    VALUES ('100024','Pasta Primavera','spinach and tomatoes')
-    VALUES ('100025','Italian Pasta','italian sausage')
-    VALUES ('100026','Alfredo Pasta','chicken')
-    VALUES ('100027','Wings','Marinated over-baked')
-    VALUES ('100028','Cheesy Bread','oven-baked')
-    VALUES ('100029','Candy Apples','apples with coating')
-    VALUES ('100030','Champagne Cake','lovely white cake') 
-    VALUES ('100031','Mummy Dogs','hotdogs')
-    VALUES ('100032','Pumpkin Smoothies','pumpkin')
-    VALUES ('100033','Chocolate Spiders','chocolate')
-    VALUES ('100034','Pumpkin Cookies','Cookies')
-    VALUES ('100035','Chip Bread Pudding','chocolaty-banana')
-    VALUES ('100036','Pumpkin Seeds','sweet')
-    VALUES ('100037','Peanut Butter','chocolate')
-    VALUES ('100038','Lemon Cookies','taste like lemon')
-    VALUES ('100039','Shortbread Cookie','chocolate-dipped')
-    VALUES ('100040','Fish Dry Pot','fish and vegetables')
-    VALUES ('100041','Sliced Flounder','chili sauce')
-    VALUES ('100042','French Fries','new baked')
-    VALUES ('100043','Cooked Scallop','dry cooked')
-    VALUES ('100044','Fries Chicken Wrap','with tomatoes')
-    VALUES ('100045','Brisket Sandwich','BBQ beef')
-    VALUES ('100046','Curry Chicken','red curry')
-    VALUES ('100047','Curry Beef','green curry')
-    VALUES ('100048','Salad','tomato and cabbage')
-    VALUES ('100049','red bean ice cream','red bean')
-    VALUES ('100050','fried banana','with ice cream')
-    
+    VALUES 
+    ('100001','Kitkat','Chocolate'),
+    ('100002','Marshmallow','Candy'),
+    ('100003','Lollipop','Candy'),
+    ('100004','Hershey','Chocolate'),
+    ('100005','Bacon','Meat'),
+    ('100006','Ribs','Meat'),
+    ('100007','Chicken','Meat'),
+    ('100008','Sausage','Meat'),
+    ('100009','Egg','Meat'),
+    ('100010','Cheese','Milk'),
+    ('100011','Snapple','Juice'),
+    ('100012','Cola','Soda'),
+    ('100013','Cookie','Home-made'),
+    ('100014','Pastas','Home-made'),
+    ('100015','Boneless-chicken','Home-made'),
+    ('100016','M&M','Candy'),
+    ('100017','Lays','Chip'),
+    ('100018','Mocha','Coffee'),
+    ('100019','Oreo','chocolate cookie'),
+    ('100020','Spinach Pizza','spinach and fita'),
+    ('100021','Honolulu Pizza','pineapple and meat'),
+    ('100022','Philly Pizza','cheese steak'),
+    ('100023','Hawaiian Pizza','jalapeno and ham'),
+    ('100024','Pasta Primavera','spinach and tomatoes'),
+    ('100025','Italian Pasta','italian sausage'),
+    ('100026','Alfredo Pasta','chicken'),
+    ('100027','Wings','Marinated over-baked'),
+    ('100028','Cheesy Bread','oven-baked'),
+    ('100029','Candy Apples','apples with coating'),
+    ('100030','Champagne Cake','lovely white cake') ,
+    ('100031','Mummy Dogs','hotdogs'),
+    ('100032','Pumpkin Smoothies','pumpkin'),
+    ('100033','Chocolate Spiders','chocolate'),
+    ('100034','Pumpkin Cookies','Cookies'),
+    ('100035','Chip Bread Pudding','chocolaty-banana'),
+    ('100036','Pumpkin Seeds','sweet'),
+    ('100037','Peanut Butter','chocolate'),
+    ('100038','Lemon Cookies','taste like lemon'),
+    ('100039','Shortbread Cookie','chocolate-dipped'),
+    ('100040','Fish Dry Pot','fish and vegetables'),
+    ('100041','Sliced Flounder','chili sauce'),
+    ('100042','French Fries','new baked'),
+    ('100043','Cooked Scallop','dry cooked'),
+    ('100044','Fries Chicken Wrap','with tomatoes'),
+    ('100045','Brisket Sandwich','BBQ beef'),
+    ('100046','Curry Chicken','red curry'),
+    ('100047','Curry Beef','green curry'),
+    ('100048','Salad','tomato and cabbage'),
+    ('100049','red bean ice cream','red bean'),
+    ('100050','fried banana','with ice cream'),
+
     
     );
